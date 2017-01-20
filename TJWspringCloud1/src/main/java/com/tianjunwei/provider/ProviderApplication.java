496a285c837fff98cef423b7f14370be62f0ea25
tiantianjunwei <924862077@qq.com>
2017-01-20 19:22:03 +0800

    修改包名

--- TJWspringCloud1/src/main/java/com/tianjunwei/provider/ProviderApplication.java
+++ TJWspringCloud1/src/main/java/com/tianjunwei/provider/ProviderApplication.java
@@ -1,4 +1,4 @@
-package com.tianjunwei.zookeeper;
+package com.tianjunwei.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
