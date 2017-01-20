0dc5e1ac8a52dc8985feed81f07bec10119b25d4
tiantianjunwei <924862077@qq.com>
2017-01-20 19:33:49 +0800

    添加服务提供者和消费者代码

---
+++ TJWspringCloudProvider/src/main/java/com/tianjunwei/provider/web/ComputeController.java
@@ -0,0 +1,24 @@
+package com.tianjunwei.provider.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ComputeController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+
+    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer r = a + b;
+        return r;
+    }
+
+}
