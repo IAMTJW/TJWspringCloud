06ae468ac2e98bfd96de4e93fb7e1508505913ca
tianjunwei <924862077@qq.com>
2018-04-19 16:24:49 +0800

    添加服务注册中心

--- TJWspringCloudServer/src/main/java/com/tianjunwei/server/RegisterApplication.java
+++ TJWspringCloudServer/src/main/java/com/tianjunwei/server/RegisterApplication.java
@@ -2,10 +2,10 @@ package com.tianjunwei.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-@EnableDiscoveryClient
 public class RegisterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterApplication.class, args);
